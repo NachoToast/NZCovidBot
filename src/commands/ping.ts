10f36f0486eea30f4f4e7a65d105000ba8987dbb
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-08-26 20:31:52 +1200

    Trim ping command.

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,15 +3,12 @@ import Command from '../interfaces/Command';
 
 const ping: Command = {
     execute: async ({ message, args, client }: { message: Message; args: string[]; client: Client }) => {
-        let response = `Pongers! (${Math.abs(Date.now() - message.createdTimestamp)}ms)`;
-        if (args.includes('v')) {
-            response += `\nAPI Latency: ${Math.round(client.ws.ping)}ms`;
-        }
-
-        message.channel.send(response);
+        message.channel.send(
+            `Pongers! (${Math.abs(Date.now() - message.createdTimestamp)}ms)\nAPI Latency: ${Math.round(client.ws.ping)}ms`
+        );
     },
     help: async ({ message }: { message: Message }) => {
-        message.channel.send(`Pings the bot to see if it's online, usage: \`covid ping\``);
+        message.channel.send(`Ping the bot to see if it's online.\nUsage: \`covid ping\``);
     },
 };
 
